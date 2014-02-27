a2f6cb8cf22fa827cb6291622b43ab93fc07cec4
Ivan Galinskiy <ivanko.rus@gmail.com>
2014-02-27 11:39:32 -0600

    Added options for 3D positions

--- run.py
+++ run.py
@@ -23,26 +23,42 @@ th_steps = 100;
 
 # The limits of integration for x (radial position of particle) IN TERMS OF
 # PARTICLE RADIUS. The particle position is relative to the focus.
-x_final = 2;
+x_init = 0;
+x_final = 1;
 x_steps = 50;
+
+# The limits of integration for x (radial position of particle) IN TERMS OF
+# PARTICLE RADIUS. The particle position is relative to the focus.
+y_init = 0;
+y_final = 0;
+y_steps = 50;
 
 # The limits of integration for z (axial position of particle) IN TERMS OF
 # PARTICLE RADIUS. The particle position is relative to the focus.
-z_init = -2;
-z_final = 2;
+z_init = 0;
+z_final = 1;
 z_steps = 50;
 
 # List of arguments
 args_list = [
 lens_r,
 df,
+
 sph_r,
 sph_ne,
 sph_n,
+
 r_steps,
 th_steps,
+
+x_init,
 x_final,
 x_steps,
+
+y_init,
+y_final,
+y_steps,
+
 z_init,
 z_final,
 z_steps
