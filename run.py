3911391c599dd276befc0e6f765b62fc3106b419
Ivan Galinskiy <ivanko.rus@gmail.com>
2014-07-17 17:25:13 -0500

    Fixed the comments in the file

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-import subprocess
+import subprocess as sp
 from math import pi
 
+# Path to the simulation executable
 exec_path = ["./app"]
 
 # Here come the arguments for the simulation
@@ -10,10 +11,10 @@ exec_path = ["./app"]
 # The lens focal distance (in m)
 df = 30e-3;
 
-# Incoming beam width
+# Incoming beam radius
 l_r = 1.1e-3;
 
-# Medium refractive index
+# External medium refractive index
 ne = 1;
 
 # Relative index of the sphere
@@ -23,37 +24,36 @@ sph_n = 1.5;
 r_steps = 300;
 th_steps = 300;
 
-# The limits of integration for x (radial position of particle) IN TERMS OF
-# PARTICLE RADIUS. The particle position is relative to the focus.
+# The limits of integration for x (radial position of particle) in meters. 
+# The particle position is relative to the focus.
 x_init = 0;
 x_final = 0;
 x_steps = 100;
 
-# The limits of integration for y (radial position of particle) IN TERMS OF
-# PARTICLE RADIUS. The particle position is relative to the focus.
+# The limits of integration for y (radial position of particle) in meters. 
+# The particle position is relative to the focus.
 y_init = 0;
 y_final = 0;
 y_steps = 3;
 
-# The limits of integration for z (axial position of particle) IN TERMS OF
-# PARTICLE RADIUS. The particle position is relative to the focus.
+# The limits of integration for z (axial position of particle) in meters. 
+# The particle position is relative to the focus.
 z_init = -1000e-6;
 z_final = 1000e-6;
 z_steps = 1003;
 
-# Double trap?
+# Double (contrapropagating) trap? 1 for yes, 0 for no
 double_trap = 0
 
-# Sphere radiuses
+# Sphere radiuses to be simulated (in meters)
 s_rl = [4e-6, 5e-6, 6e-6, 7e-6, 8e-6, 9e-6, 10e-6, 11e-6, 12e-6]
 
 # Light wavelength
 lam = 532e-9 
 
-# List of arguments
-
 cmd_l = []
 
+# Generate the commands
 for s_r in s_rl:
     args_list = [
     df,
