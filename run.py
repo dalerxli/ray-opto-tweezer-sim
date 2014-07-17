d4a59317ad93587dc4d67cfb00b3bbf94b4cfbd1
Ivan Galinskiy <ivanko.rus@gmail.com>
2014-07-17 17:47:24 -0500

    Now the run.py file runs the executable itself.

--- run.py
+++ run.py
@@ -4,7 +4,10 @@ import subprocess as sp
 from math import pi
 
 # Path to the simulation executable
-exec_path = ["./app"]
+exec_path = "./app"
+
+# Output file (to write the simulation results)
+out_path = "output.tsv"
 
 # Here come the arguments for the simulation
 
@@ -48,14 +51,15 @@ double_trap = 0
 # Sphere radiuses to be simulated (in meters)
 s_rl = [4e-6, 5e-6, 6e-6, 7e-6, 8e-6, 9e-6, 10e-6, 11e-6, 12e-6]
 
-# Light wavelength
+# Light wavelength in meters
 lam = 532e-9 
-
-cmd_l = []
 
 # Generate the commands
 for s_r in s_rl:
+    # Create the argument list for subprocess, including the executable itself
     args_list = [
+    exec_path,
+    
     df,
     l_r,
     ne,
@@ -82,10 +86,24 @@ for s_r in s_rl:
     s_r,
     lam,
     ]
+    
+    out_file = open(out_path, "a")
 
     args_list = map(lambda x: str(x), args_list)
-    cmd = "echo -e \'\\n\\n\"Radius {0:.1f} um\"\' >> mul2.tsv && ".format(s_r*1e6)
-    cmd += " ".join(exec_path+args_list)
-    cmd += " >> mul2.tsv"
-    cmd_l.append(cmd)
-print " && ".join(cmd_l)
+    
+    # Write the current particle radius for plotting software
+    out_file.write('"Radius {0:.1f} um"\n'.format(s_r*1e6))
+    
+    # Make sure that the above is written
+    out_file.flush()
+    
+    # Call the executable and wait for it to complete
+    sp.call(args_list, stdout=out_file)
+    
+    out_file.flush()
+    out_file.write("\n\n")
+    
+    print "Particle radius {0:.1f} um finished".format(s_r*1e6)
+    
+    # Close the file to write changes
+    out_file.close()
