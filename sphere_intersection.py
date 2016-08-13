5ba1d40b83af4e80daf3a1a3cc092435d863b52b
Ivan Galinskiy <ivanko.rus@gmail.com>
2016-08-12 23:10:26 -0500

    Fixed a typo in the discriminant (ex-catastrophic failure) and in the dot product. Normal-incidence tests passing

--- sphere_intersection.py
+++ sphere_intersection.py
@@ -1,34 +1,49 @@
 import numpy as np
 import numpy.linalg as npl
 
+import logging
+
+logging.basicConfig(level=logging.INFO)
+lgg = logging.getLogger("intersection_module")
+
 def intersection_angle(c, R, o, l):
     # Make l (director of the line) unitary
-    l = l/npl.norm(l)
+    ln = l/npl.norm(l)
     
     # Calculate the discriminant (to see whether there are any solutions)
-    D = -np.dot(l, (o-c))**2 - np.dot((o-c), (o-c)) + R**2
+    D = np.dot(ln, (o-c))**2 - np.dot((o-c), (o-c)) + R**2
+    lgg.debug(D)
     
     # If there are no solutions, then there is nothing else to do
     if D < 0:
         return np.nan
     
     # Otherwise, calculate the distance along the line where an intersection occurs (doesn't matter which since this is a sphere)
-    d = -np.dot(l, (o-c)) + np.sqrt(D)
+    d = -np.dot(ln, (o-c)) + np.sqrt(D)
+    lgg.debug(d)
     
     # The point at which the intersection occurs is x:
-    x = o + d*l
+    x = o + d*ln
     
     # If x is zero (which would be a problem when calculating its inverse norm), switch to the other point:
     if np.all(x == np.array([0,0,0])):
-        d = -np.dot(l, (o-c)) - np.sqrt(D)
-        x = o + d*l
+        d = -np.dot(ln, (o-c)) - np.sqrt(D)
+        x = o + d*ln
+        
+    lgg.debug(x)
     
     # The vector that points from the center of the sphere to the intersection is r:
     r = x-c
+    lgg.debug(r)
     
-    # To find the angle of intersecting ray with the normal to the surface, first find the cosine of that angle
-    c_angle = np.dot(x, r)/(npl.norm(x)*npl.norm(r))
+    # To find the angle of intersecting ray with the normal to the surface, first find the cosine of that angle (absolute value of it)
+    c_angle = np.abs(np.dot(ln, r)/(npl.norm(ln)*npl.norm(r)))
+    lgg.debug(c_angle)
     
     # And finally, return the angle (absolute value)
+    if c_angle <= 1:
+        pass
+    elif (c_angle - 1) < 1e-5:
+        c_angle = 1
     return np.arccos(np.abs(c_angle))
     
--- tests/test_sphere_intersection.py
+++ tests/test_sphere_intersection.py
@@ -7,9 +7,9 @@ import sphere_intersection as ix
 # Auxiliary
 import numpy as np
 
+# The function should find the intersection between a sphere (centered at c, the first argument) of radius R (second argument) with a line of origin o (third argument) and direction vector l (fourth argument)
 class SphereIntersectionTestCase(unittest.TestCase):
     def test_intersect_normal(self):
-        # The function should find the intersection between a sphere (centered at c, the first argument) of radius R (second argument) with a line of origin o (third argument) and direction vector l (fourth argument)
         c = np.array([0,0,0])
         R = 1
         o = np.array([0,0,0])
@@ -18,9 +18,44 @@ class SphereIntersectionTestCase(unittest.TestCase):
         angle = ix.intersection_angle(c, R, o, l)
         self.assertAlmostEqual(angle, 0)
         
+    def test_intersect_normal_offset(self):
         # Let's now offset the sphere and the line origin
-        c = np.array([1,0,0])
-        o = np.array([1,0,0])
+        c = np.array([3,0,0])
+        o = np.array([3,0,0])
+        R = 1
+        l = np.array([1,0,0])
+        
+        angle = ix.intersection_angle(c, R, o, l)
+        self.assertAlmostEqual(angle, 0)
+    
+    def test_intersect_normal_inv_director(self):
+        c = np.array([3,0,0])
+        o = np.array([3,0,0])
+        R = 1
+        l = np.array([1,0,0])
+        
+        # Inverting the line director shouldn't have any effect:
+        l = -6*l
+        
+        angle = ix.intersection_angle(c, R, o, l)
+        self.assertAlmostEqual(angle, 0)
+        
+    def test_intersect_normal_angle_director(self):
+        # A director can go in any direction from the center of the sphere without changing the incidence angle
+        c = np.array([3,0,0])
+        o = np.array([3,0,0])
+        R = 1
+        l = np.array([1,1,1])
+        
+        angle = ix.intersection_angle(c, R, o, l)
+        self.assertAlmostEqual(angle, 0)
+        
+    def test_intersect_normal_angle_director_external(self):
+        # And now let the line hit the sphere normally, but at an angle to an axis
+        c = np.array([5,0,0])
+        o = np.array([0,0,5])
+        l = np.array([1,0,-1])
+        R = 1
         
         angle = ix.intersection_angle(c, R, o, l)
         self.assertAlmostEqual(angle, 0)
@@ -34,6 +69,15 @@ class SphereIntersectionTestCase(unittest.TestCase):
         
         angle = ix.intersection_angle(c, R, o, l)
         self.assertAlmostEqual(angle, 0)
+        
+    def test_intersect_tangent(self):
+        c = np.array([3,0,0])
+        R = 1
+        o = np.array([4,0,10])
+        l = np.array([0,0,-1])
+        
+        angle = ix.intersection_angle(c, R, o, l)
+        self.assertAlmostEqual(angle, np.pi/2)
         
 if __name__ == '__main__':
     unittest.main()
