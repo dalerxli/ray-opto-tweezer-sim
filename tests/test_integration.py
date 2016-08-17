fa51e3f09c614ddb998dde53c53aad72efaaa8f8
Ivan Galinskiy <ivanko.rus@gmail.com>
2016-08-17 18:05:13 -0500

    The test is now failing properly, lol

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,15 +1,31 @@
 # Testing rig
 import unittest
 
-# Module to test
+# Modules to test
 import sphere_intersection as ix
+import lens_integration as lint
 
 # Auxiliary
 import numpy as np
 import numpy.linalg as npl
 
+# For integration
+import scipy.integrate as si
+
 # This class tests integration over all the rays coming out of a lens
 class TestIntegration(unittest.TestCase):
-    # Test the power coming out of lens with a uniform fill. This power has to be equal to unity (a check on the radial integration)
+    # Test the power coming out of lens with a uniform fill. This power has to be equal to unity (a check on the polar integration)
     def test_integrate_uniform(self):
-        pass
+        # Radius of the lens
+        R = 1e-3
+        
+        # A helper function to call the function from lens_integration since that function requires more arguments
+        def unif_inten(r, th):
+            # The target function should return the intensity multiplied by r (for polar integration) as the first element
+            return lint.unif(r, th, R)[0]
+        
+        int_result = si.dblquad(unif_inten, 0, R, lambda x: 0, lambda x: 2*pi)
+        self.assertAlmostEqual(int_result, 1)
+        
+if __name__ == '__main__':
+    unittest.main()
