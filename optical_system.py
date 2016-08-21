Found a big mistake in polarization projection calculation

optical_system.py
@@ -128,9 +128,9 @@ class OpticalSystem(object):
         
         dir_grad = a - dot_rows(a, dir_scat).reshape(-1,1)*dir_scat
         
-        # If the rays pass through the center of the sphere, then dir_grad will be = 0, but this is not a problem as this ray will not exert any gradient force. Then, we can take any direction as dir_grad without any consequence:
-        #if not np.all(dir_grad == np.array([0,0,0])):
-            #dir_grad = dir_grad / npl.norm(dir_grad)
+        # If the rays pass through the center of the sphere, then dir_grad will be = 0, but this is not a problem as this ray will not exert any gradient force. Then, we can take any direction as dir_grad without any consequence. Otherwise, we have to normalize dir_grad
+        if not np.all(dir_grad == np.array([0,0,0])):
+            dir_grad = dir_grad / npl.norm(dir_grad, axis=1).reshape(-1,1)
             
         # The magnitudes of the forces are specified in Ashkin, 1992. First let's calculate some auxiliary quantities:
         
@@ -143,7 +143,7 @@ class OpticalSystem(object):
         pn = dot_rows(p, p)
         
         # Then, use it to calculate the projection of the polarization on the incidence plane
-        Pp = np.abs(dot_rows(p, dir_grad))**2 + np.abs(dot_rows(p, dir_scat))**2/pn
+        Pp = (np.abs(dot_rows(p, dir_grad))**2 + np.abs(dot_rows(p, dir_scat))**2)/pn
         
         # Sometimes, the proportion will be slightly bigger than 1 because of floating-point errors. The following corrects it:
         Pp[(Pp > 1) & (Pp < 1+1e-7)] = 1

tests/test_system.py
@@ -207,8 +207,8 @@ class SphereIntersectionForceTestCase(unittest.TestCase):
         
         forces = opt._ray_force(p)
         
-        # There should be a force towards +x
-        self.assertGreater(forces[0,0], 0)
+        # There should be a force towards +x. Moreover, it should more than 0.04 (reflectivity)
+        self.assertGreater(forces[0,0], 0.04)
         
         # And the forces in the other directions should be zero
         self.assertTrue(np.allclose(forces[0,1:], 0))
@@ -251,7 +251,7 @@ class SphereIntersectionForceTestCase(unittest.TestCase):
         opt._c = np.array([c])
         
         # And this is the polarization of the ray in Jones notation (circular)
-        p = np.array([[1,1j,0]])/np.sqrt(2)
+        p = np.array([[1,1j,0]])
         
         data = np.array([
             [1.1, np.sqrt(0.429**2 + 0.262**2), 79*np.pi/180],
@@ -292,17 +292,17 @@ class TestIntegration(unittest.TestCase):
         rp = 5e-6
         
         # And the polarization is linear
-        p = np.array([1,0,0])
+        p = np.array([1,1j,0])
         
         opt = osys.OpticalSystemSimpleUniform(np.array([0,0,0]), rp, 1.5, Rl, f, p)
         
         # Data from Ashkin, 1992
         data = np.array([
-            [1.2, 0.00, 0.00, 1.01*rp, -0.276, 2],
+            #[1.2, 0.00, 0.00, 1.01*rp, -0.276, 2],
             #[1.2, 0.00, 0.98*rp, 0.00, -0.313, 1],
             #[1.2, 1.05*rp, 0.00, 0.00, -0.490, 0],
-            #[1.4, 0.00, 0.00, 0.93*rp, -0.282, 2],
-            #[1.8, 0.00, 0.00, 0.88*rp, -0.171, 2]
+            [1.4, 0.00, 0.00, 0.93*rp, -0.282, 2],
+            [1.8, 0.00, 0.00, 0.88*rp, -0.171, 2]
             ])
         
         def check(row):