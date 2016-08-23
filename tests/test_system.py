5a4168d0ae4a5f10ed936d0a7e74aa24a77a6fb6
Ivan Galinskiy <ivanko.rus@gmail.com>
2016-08-23 00:45:59 -0500

    Gaussian beam implemented and tests are passing

--- tests/test_system.py
+++ tests/test_system.py
@@ -284,7 +284,7 @@ class SphereIntersectionForceTestCase(unittest.TestCase):
         
 ## This class tests integration over all the rays coming out of a lens
 class TestIntegration(unittest.TestCase):
-    # Finally, compare the total forces on the sphere to some of Ashkin's results
+    # Finally, compare the total forces on the sphere to some of Ashkin's results (for uniformly-filled objective)
     def test_force_uniform(self):
         f = 1e-3
         
@@ -328,3 +328,47 @@ class TestIntegration(unittest.TestCase):
         t1 = dt.datetime.now()
         print(t1-t0)
         self.assertLess(np.max(res), 0.02)
+        
+    # Finally, compare the total forces on the sphere to some of Ashkin's results (for uniformly-filled objective)
+    def test_force_gaussian(self):
+        f = 1e-3
+        
+        # A microscope objective with NA = 1.25 (water-immersion). The half-angle of convergence is about 70 degrees
+        Rl = f * np.tan(np.arcsin(1.25/1.33))
+        
+        # A particle of rp=5e-6. Not necessary in this case, but I'll keep for consistency.
+        rp = 5e-6
+        
+        # And the polarization is linear
+        p = np.array([1,1j,0])
+        
+        # Data from Ashkin, 1992
+        data = np.array([
+            [1.2, 0.00, 0.00, 1.01*rp, -0.276, 2, 1e6*Rl], # Limit of uniformly-filled objective (consistency check)
+            [1.2, 0.00, 0.98*rp, 0.00, -0.313, 1, 1e6*Rl], # Limit of uniformly-filled objective (consistency check)
+            [1.2, 0.00, 0.00, 1.01*rp, -0.259, 2, 1.7*Rl],
+            [1.2, 0.00, 0.98*rp, 0.00, -0.326, 1, 1.7*Rl],
+            [1.2, 0.00, 0.98*rp, 0.00, -0.349, 1, 1.0*Rl]
+            ])
+        
+        def check(row):
+            n = row[0]
+            pos = row[1:4]
+            targetQ = row[4]
+            
+            # Force index to check
+            i = row[5]
+            
+            # The Gaussian beam waist (~infinity for uniform filling)
+            a = row[6]
+            
+            opt = osys.OpticalSystemSimpleUniform(pos, rp, n, Rl, f, p)
+            force = opt.integrate(200, 200)
+            
+            return np.abs(force[int(i)] - targetQ)
+            
+        t0 = dt.datetime.now()
+        res = np.apply_along_axis(check, axis=1, arr=data)
+        t1 = dt.datetime.now()
+        print(t1-t0)
+        self.assertLess(np.max(res), 0.03)
