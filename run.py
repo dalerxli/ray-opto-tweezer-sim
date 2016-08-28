aed81b70582537ab0f00c1b1e2703f5d0a399b1d
Ivan Galinskiy <ivanko.rus@gmail.com>
2016-08-28 16:15:10 -0500

    Began using the config file in run.py. Testing to be done

--- run.py
+++ run.py
@@ -3,38 +3,68 @@
 import optical_system as osys
 import numpy as np
 
+# Imports for loading configuration files
+import configparser
+from ast import literal_eval as make_tuple # To easily convert strings to tuples
+config_file = "config.ini"
+
+config = configparser.ConfigParser()
+config.read(config_file)
+
+
 # Output file
-out_file = "results-2d.tsv"
+out_file = config["Config"]["out_file"]
 
 # The radius of the particle will be taken as unity with no loss of generality.
-Rp = 1.0
-n = 1.47 # Glycerol
+Rp = config["Particle"].getfloat("radius")
+n = config["Particle"].getfloat("nr")
 
 # The NA will be used for calculating the lens radius, though the pair lens radius-focal length can also be specified below
 # Note: if the NA is for liquid-immersion objective, then it should be divided by the index of that liquid (so that it's a number less than 1)
-NA = 0.68 # Thorlabs/Geltech C330TMD-A
+NA = config["Beam"].getfloat("NA") # Thorlabs/Geltech C330TMD-A
 
-f = 1e3 # A lot to guarantee that the particle doesn't hit the lens (nothing horrible should happen, but still)
+f = 1e5*Rp # A lot to guarantee that the particle doesn't hit the lens (nothing horrible should happen, but still)
 
 # Finally, calculate the lens radius
 Rl = f * np.tan(np.arcsin(NA))
 
 # And with it, the beam radius
-a = Rl * 1.0
+a = Rl * config["Beam"].getfloat("a")
 
-# Polarization vector. Only the first two components are to be different from 0
-p = np.array([1,0,0])
+# Polarization vector
+pi = make_tuple(config["Beam"]["p"])
+p = np.append(np.array(pi), 0)
 
 # The first argument is the lowest x, the second is the highest x and the third is the number of steps to take.
 # If not iterating over x, then set it to 0, 0, 1 (or change 0 to the desired fixed value of x)
 # All the coordinates are zero when the particle is at the focus. Z decreases when the particle is closer to the lens.
-xs = np.linspace(0, 2, 100)
+ranges = config["Positions"]
+
+xstart = ranges.getfloat("xstart")
+xstop = ranges.getfloat("xstop")
+xsteps = ranges.getfloat("xsteps")
+
+if xstart == xstop:
+    xsteps = 1
+xs = np.linspace(xstart, xstart, xsteps)
 
 # Same as above, for y
-ys = np.linspace(0, 0, 1)
+ystart = ranges.getfloat("ystart")
+ystop = ranges.getfloat("ystop")
+ysteps = ranges.getfloat("ysteps")
+
+if ystart == ystop:
+    ysteps = 1
+ys = np.linspace(ystart, ystart, ysteps)
 
 # Same as above, for z
-zs = np.linspace(-2, 2, 100)
+zstart = ranges.getfloat("zstart")
+zstop = ranges.getfloat("zstop")
+zsteps = ranges.getfloat("zsteps")
+
+if zstart == zstop:
+    zsteps = 1
+zs = np.linspace(zstart, zstart, zsteps)
 
 # Now we generate the space of all the necessary coordinates, where every row is a position to be calculated
 xx, yy, zz = np.meshgrid(xs, ys, zs)
@@ -47,7 +77,7 @@ opt = osys.OpticalSystemSimpleGaussian(np.array([0,0,0]), Rp, n, Rl, f, p, a)
 # An auxiliary function for applying on each row
 def force(row):
     opt.set_particle_center(row)
-    force = opt.integrate(200, 200)
+    force = opt.integrate(config["Integration"].getint("rsteps"), config["Integration"].getint("thsteps"))
     
     return force
 
